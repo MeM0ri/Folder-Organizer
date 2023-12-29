f2b7c70e43a1e0194a2c4c0783ceb80903f9ff96
Aleksey Fokin <augustwinterart@gmail.com>
2023-12-30 02:51:08 +0300

    Add undo feature

--- FolderOrganizer.py
+++ FolderOrganizer.py
@@ -8,17 +8,27 @@ def create_logger():
     logging.basicConfig(filename='file_organizer.log', level=logging.INFO, format='%(asctime)s: %(message)s')
     return logging.getLogger()
 
-def move_file(file_path, target_directory, logger, dry_run=False):
+def move_file(file_path, target_directory, logger, dry_run=False, move_records=None):
     if dry_run:
         logger.info(f"[DRY RUN] Would move file {file_path} to {target_directory}")
     else:
         try:
             shutil.move(file_path, target_directory)
             logger.info(f"Move file {file_path} to {target_directory}")
+            if move_records is not None:
+                move_records.append((target_directory, file_path))  #Recorde file move for undo feature
         except Exception as e:
             logger.error(f"Error moveing file {file_path} to {target_directory}: {e}")
 
-def organize_by_type(directory, logger, dry_run):
+def undo_last_operation(move_records, logger):
+    for target_directory, original_path in reversed(move_records):
+        try:
+            shutil.move(os.path.join(target_directory, os.path.basename(original_path)), original_path)
+            logger.info(f"Reversed move: {original_path} back to {target_directory}")
+        except Exception as e:
+            logger.error(f"Error reversing file move from {target_directory} to {original_path}: {e}")
+
+def organize_by_type(directory, logger, dry_run, move_records):
     for filename in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, filename)):
             file_type = filename.split('.')[-1]
@@ -28,9 +38,9 @@ def organize_by_type(directory, logger, dry_run):
                 os.makedirs(target_directory)
 
             file_path = os.path.join(directory, filename)
-            move_file(file_path, target_directory, logger, dry_run)
+            move_file(file_path, target_directory, logger, dry_run, move_records)
 
-def organize_by_date(directory, logger, dry_run):
+def organize_by_date(directory, logger, dry_run, move_records):
     for filename in os.listdir(directory):
         if os.path.isfile(os.path.join(directory, filename)):
             file_creation_time = os.path.getctime(os.path.join(directory, filename))
@@ -41,17 +51,23 @@ def organize_by_date(directory, logger, dry_run):
                 os.makedirs(target_directory)
 
             file_path = os.path.join(directory, filename)
-            move_file(file_path, target_directory, logger, dry_run)
+            move_file(file_path, target_directory, logger, dry_run, move_records)
 
 def run_terminal_mode(args):
     logger = create_logger()
 
+    move_records = []
+
     if args.sort == 'type':
-        organize_by_type(args.directory, logger, args.dry_run)
+        organize_by_type(args.directory, logger, args.dry_run, move_records)
     else:
-        organize_by_date(args.directory, logger, args.dry_run)
+        organize_by_date(args.directory, logger, args.dry_run, move_records)
 
     print("File organization completed with Terminal mode!")
+
+    if move_records and input("Do you want to undo the last operation? (y/n): ").lower() == 'y':
+        undo_last_operation(move_records, logger)
+        print("Last operation has been undone.")
             
 def run_cli_mode():
     print("I'll help you to organize your directory.")
@@ -78,15 +94,21 @@ def run_cli_mode():
 
     confirm = input("Procede with file organization? (y/n): ")
 
+    move_records = []
+
     if confirm.lower() == 'y':
         logger = create_logger()
         if sort_method == 'type':
-            organize_by_type(directory_path, logger, dry_run)
+            organize_by_type(directory_path, logger, dry_run, move_records)
         elif sort_method == 'date':
-            organize_by_date(directory_path, logger, dry_run)
+            organize_by_date(directory_path, logger, dry_run, move_records)
         print("File organization completed with CLI mode!")
     else:
         print("File organization canceled.")
+
+    if move_records and input("Do you want to undo the last operation? (y/n): ").lower() == 'y':
+        undo_last_operation(move_records, logger)
+        print("Last operation has been undone.")
 
 def main():
     parser = argparse.ArgumentParser(description='Organize files in a directory.')
